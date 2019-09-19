4b5df9783062f9c6a16a153037bcb59e5b4aeeee
Erfan Abdi <erfangplus@gmail.com>
2019-09-19 18:58:02 +0200

    X00T: prop set radio felxmap to none
    Signed-off-by: BabluS <baaswanthmadhav@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -301,6 +301,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.ril.ecclist=112,911 \
     ro.telephony.default_network=20,20 \
     telephony.lteOnCdmaDevice=1
+    persist.vendor.radio.flexmap_type=none
 
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
