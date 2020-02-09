28cc16c4d8d8b52463516f39ddc80df4706c3b40
Rahul Krishna <Rahulkrishna585@gmail.com>
2020-02-09 21:40:26 +0530

    X00T: Update config.fs from LA.UM.8.2.r1-05700-sdm660.0
    Change-Id: I504629a9b9544023f0813e24d005ce0676f2c7a6

--- config.fs
+++ config.fs
@@ -43,6 +43,18 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: BLOCK_SUSPEND NET_ADMIN
 
+[vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti-lazy]
+mode: 0755
+user: AID_BLUETOOTH
+group: AID_BLUETOOTH
+caps: BLOCK_SUSPEND NET_ADMIN
+
+[system/vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti-lazy]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: BLOCK_SUSPEND NET_ADMIN
+
 [system/bin/cnss-daemon]
 mode: 0755
 user: AID_BLUETOOTH
@@ -93,8 +105,8 @@ caps: NET_BIND_SERVICE
 
 [system/vendor/bin/imsdatadaemon]
 mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
+user: AID_RADIO
+group: AID_RADIO
 caps: NET_BIND_SERVICE
 
 [vendor/bin/ims_rtp_daemon]
@@ -105,7 +117,7 @@ caps: NET_BIND_SERVICE
 
 [system/vendor/bin/ims_rtp_daemon]
 mode: 0755
-user: AID_SYSTEM
+user: AID_RADIO
 group: AID_RADIO
 caps: NET_BIND_SERVICE
 
@@ -117,7 +129,7 @@ caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
 
 [system/vendor/bin/imsrcsd]
 mode: 0755
-user: AID_SYSTEM
+user: AID_RADIO
 group: AID_RADIO
 caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
 
